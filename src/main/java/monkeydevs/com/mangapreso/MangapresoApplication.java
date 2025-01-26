cb5132a139b0de0f49251703f16f9f0de9813e16
viniciusventura29 <viniciusventura29@gmail.com>
2025-01-26 01:29:44 -0300

    init project

---
+++ src/main/java/monkeydevs/com/mangapreso/MangapresoApplication.java
@@ -0,0 +1,13 @@
+package monkeydevs.com.mangapreso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangapresoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangapresoApplication.class, args);
+	}
+
+}
